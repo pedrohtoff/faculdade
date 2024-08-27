879b8d4a65e6dba2abb0609d6fb7fe8687720807
pedrohtoff <pedrohtoff@gmail.com>
2024-08-27 15:21:23 -0400

    Criação de Classes e start na classe parametro

---
+++ POO1/projetoEstoque/projetoEstoque/src/domain/BaseParametro.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.time.LocalDate;
+
+public abstract class BaseParametro {
+    protected int codigo;
+    protected String descricao;
+    protected LocalDate dataInclusao;
+
+    public BaseParametro() {
+    }
+
+    public BaseParametro(int codigo, String descricao, LocalDate dataInclusao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.dataInclusao = dataInclusao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInclusao() {
+        return dataInclusao;
+    }
+
+    public void setDataInclusao(LocalDate dataInclusao) {
+        this.dataInclusao = dataInclusao;
+    }
+
+}
