560bfec08dbc581b55180e4462522cc6a04e72b1
pedrohtoff <pedrohtoff@gmail.com>
2024-08-27 15:35:47 -0400

    Criação da view

--- POO1/projetoEstoque/projetoEstoque/src/view/ClaseProdutoVisao.java
+++ POO1/projetoEstoque/projetoEstoque/src/view/ClaseProdutoVisao.java
@@ -1,5 +1,21 @@
 package view;
 
-public class ClaseProdutoVisao {
+import java.time.LocalDate;
 
+import domain.ClasseProduto;
+
+public class ClaseProdutoVisao {
+    public void Exibir() {
+        ClasseProduto cp1 = new ClasseProduto(1, "Carnes", LocalDate.now());
+        System.out.println("Classe de Produto 1: ");
+        System.out.println("Codigo: " + cp1.getCodigo());
+        System.out.println("Descrição: " + cp1.getDescricao());
+        System.out.println("Data de inclusão: " + cp1.getDataInclusao());
+
+        ClasseProduto cp2 = new ClasseProduto(2, "Bebidas", LocalDate.now());
+        System.out.println("Classe de Produto 2: ");
+        System.out.println("Codigo: " + cp2.getCodigo());
+        System.out.println("Descrição: " + cp2.getDescricao());
+        System.out.println("Data de inclusão: " + cp2.getDataInclusao());
+    }
 }
