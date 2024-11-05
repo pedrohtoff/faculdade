ba31178c0f84367b0dfb1cf0aed1079e3cd482c7
Pedro Henrique Toffanetto <pedrohtoff@gmail.com>
2024-11-05 16:15:16 -0400

    Delete POO1/TrabalhoPraticoN2 directory

--- POO1/TrabalhoPraticoN2/src/App.java
+++
@@ -1,8 +0,0 @@
-import view.LivroMenu;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        LivroMenu menu = new LivroMenu();
-        menu.ExibirMenu();
-    }
-}
--- POO1/TrabalhoPraticoN2/src/view/Util.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-public class Util {
-    public static void LimparConsole() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-}
